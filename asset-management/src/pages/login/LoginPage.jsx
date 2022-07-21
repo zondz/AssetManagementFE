a459356d8a28f67fb1e97e97c2dff1d6c33a10c5
zondz <1801040203@s.hanu.edu.vn>
2022-07-21 12:31:51 +0700

    login page no logic
    login page no logic

--- asset-management/src/pages/login/LoginPage.jsx
+++ asset-management/src/pages/login/LoginPage.jsx
@@ -1,12 +1,80 @@
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Form, Input, Button } from "antd";
+import "antd/dist/antd.min.css";
+import { useRef, useState } from "react";
+import "./login.css";
 
 
-function LoginPage(){
-    
+function LoginPage() {
+const [form] = Form.useForm();
 
-    return(
-        <>Login page</>
-    )
+const [login,setLogin]=useState({
+    username : '',
+    password : '',
+})
 
+const handleChange = (event)=>{
+    setLogin({
+        ...login,
+        [event.target.name]:event.target.value
+    })
 }
 
-export default LoginPage
+// console.log("state : ",login)
+ 
+  const onFinish =  async (value)=>{
+      console.log("value " ,value)
+  }
+
+
+
+
+  return (
+    
+
+    <section className="login-page">
+   
+
+      <h1>Login</h1>
+      <Form
+            onFinish={onFinish}
+            className="login-form"
+      >
+        <Form.Item
+         name="username"
+        >
+        <Input
+          name="username"
+          onChange={handleChange}
+          size="large"
+          placeholder="Username"
+          prefix={<UserOutlined />}
+        />
+        </Form.Item>
+        <Form.Item
+        name="password"
+        >
+        <Input
+        name="password"
+        onChange={handleChange}
+          size="large"
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+        </Form.Item>
+        <Form.Item >
+        <div className="center-button">
+        <Button type="primary" htmlType="submit" disabled={(!login.username)||(!login.password)}
+        >
+          Log in
+        </Button>
+        </div>
+        </Form.Item>
+      </Form>
+    
+    </section>
+  );
+}
+
+export default LoginPage;
