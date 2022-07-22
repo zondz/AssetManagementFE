8d3a1d9c2514252c64762c5b744925b40fb0c8d6
Hai Duong <106789880+duongyn@users.noreply.github.com>
2022-07-23 00:49:12 +0700

    Remove old

--- asset-management/src/services/index.js
+++
@@ -1,9 +0,0 @@
-
-import httpClient from "../httpClient/httpClient"
-import { END_POINT } from "../httpClient/config"
-export const authenticate=(data)=>{
-    console.log("service authen data : ",data)
-
-    return httpClient.login(`${END_POINT.login}`,data)
-}
-
