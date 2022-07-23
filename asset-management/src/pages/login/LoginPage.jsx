5712e41208a9a3cce0c74768644f4d99a1b22a66
zondz <1801040203@s.hanu.edu.vn>
2022-07-23 20:52:04 +0700

    add common error login handler
    add common error login handler

--- asset-management/src/pages/login/LoginPage.jsx
+++ asset-management/src/pages/login/LoginPage.jsx
@@ -60,6 +60,17 @@ function LoginPage() {
                 progress: undefined,
                 });
         }
+        else{
+            toast.error(`${error.response.data.message}`, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+        }
     }
 
 
